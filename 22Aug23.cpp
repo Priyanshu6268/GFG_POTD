024a5a2e6cfad67f37595236183c87db76b15a3d
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2023-08-22 07:37:08 +0530

    Create 22Aug23.cpp

---
+++ 22Aug23.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+    public:
+    //Function to find minimum number of operations that are required 
+    //to make the matrix beautiful.
+    int findMinOpeartion(vector<vector<int> > matrix, int n)
+    {
+        // code here 
+         int numOfOp = 0;
+        
+        multiset<int> rowSum;
+        for(int i = 0 ; i < n ; i++)
+        {
+            int temp = accumulate(matrix[i].begin(), matrix[i].end(), 0);
+            rowSum.insert(temp);
+        }
+        
+        int colSum = 0;
+        for(int j = 0 ; j < n ; j++)
+        {
+            int tempSum = 0;
+            for(int i = 0 ; i < n ;i++)
+            {
+                tempSum += matrix[i][j];
+            }
+            colSum = max(colSum,tempSum);
+        }
+        int m = max(*rowSum.rbegin(),colSum);
+        for(auto it = rowSum.begin() ; it != rowSum.end() ;it++)
+        {
+            numOfOp += m - *it;
+        }
+        return numOfOp;
+    } 
+};
