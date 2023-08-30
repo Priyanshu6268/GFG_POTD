ae0f45e071f7d7153c57f93d579de8b18ae6bbbd
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2023-08-30 21:29:57 +0530

    Create 30Aug23.cpp

---
+++ 30Aug23.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    Node* deleteNode(Node *head, int x) {
+        Node *toDelete;
+        
+        if (x == 1) {
+            toDelete = head;
+            head = head->next;
+        } else {
+            Node* itr = head;
+            --x;
+            while (--x) 
+                itr = itr->next;
+           
+            toDelete = itr->next;
+            itr->next = itr->next->next;
+        }
+       
+        free(toDelete);
+        return head;
+    }
+};
