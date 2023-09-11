f3168a38c17b1dcf788684bfb678700291db7d28
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2023-09-11 20:22:59 +0530

    Create 11Sep23.cpp

---
+++ 11Sep23.cpp
@@ -0,0 +1,23 @@
+//User-function template for C++
+
+// Complete the function
+// n: Input n
+// Return True if the given number is a lucky number else return False
+
+class Solution{
+public:
+    bool isLucky(int n) {
+        // code here
+         int x = 2;
+        
+        while(x <= n){
+            if(n % x == 0)
+                return 0;
+                
+            n -= n / x;
+            ++x;
+        }
+        
+        return 1;
+    }
+};
