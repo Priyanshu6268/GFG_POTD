7a2707acbd2bec86692cc4566915e64e874192e1
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-04-23 10:15:48 +0530

    Create 23April24.cpp

---
+++ 2024/04 - April/23April24.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int firstElement(int n) {
+        // code here
+         int mod= 1000000007;
+        int a=1;
+        int b=1;
+        int c=0;
+        
+        for(int i=2;i<n;i++){
+            c= (a+b)%mod;
+            a=b%mod;
+            b=c%mod;
+            
+        }
+        if(n<=2){
+            return 1;
+        }
+        return c;
+    }
+};
