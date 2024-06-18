337a004024aec53af381dd03bd6a6fffbeaf062b
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-06-18 11:09:38 +0530

    Create 18June24.cpp

---
+++ 2024/06 - June/18June24.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int rectanglesInCircle(int r) {
+        // code here
+        int count=0;
+        
+        for(int l=1;l<2*r;l++){
+            for(int w=1;w<2*r;w++){
+                if(l*l+w*w<=4*r*r){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
