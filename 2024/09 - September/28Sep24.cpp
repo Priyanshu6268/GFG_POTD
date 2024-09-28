41b7cb2af13012674a41c504483b804d37e7c69c
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-09-28 07:28:20 +0530

    Create 28Sep24.cpp

---
+++ 2024/09 - September/28Sep24.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int minimizeCost(int k, vector<int>& arr) {
+        // Code here
+        int n=arr.size();
+        vector<int>dp(n,1e9);
+        dp[0]=0;
+        for(int i=0;i<n;i++){
+            for(int j=i-k;j<i;j++){
+                if(j>=0)
+                dp[i]=min(dp[i],dp[j]+abs(arr[i]-arr[j]));
+            }
+        }
+        return dp[n-1];
+    }
+};
