39edd70add64e8b1b52570f984945e8d53760a8a
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2024-10-12 21:52:36 +0530

    Create 12Oct24.cpp

---
+++ 2024/10 - October/12Oct24.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int pairWithMaxSum(vector<int>& arr) {
+        // code here
+        if(arr.size()<2){
+            return -1;
+        }
+        int max=0;
+        for(int j=0;j<arr.size()-1;j++){
+            if(arr[j]+arr[j+1]>max){
+                    max=(arr[j]+arr[j+1]);
+                }
+            }
+        
+        return max;
+    }
+};
