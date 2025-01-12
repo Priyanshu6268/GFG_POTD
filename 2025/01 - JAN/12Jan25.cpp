a8e71170c46477301f7f8f1745ffce0831dc4f19
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-01-12 18:47:00 +0530

    Create 12Jan25.cpp

---
+++ 2025/01 - JAN/12Jan25.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int maxWater(vector<int> &arr) {
+        // code here
+        int totalWater = 0;
+        int lmax=0 , rmax=0;
+        int i=0;
+        int j=arr.size()-1;
+        while(i<j){
+            lmax = max(lmax , arr[i]);
+            rmax = max(rmax , arr[j]);
+            if(arr[i]<=arr[j]){
+                totalWater += lmax - arr[i];
+                i++;
+            }
+            else{
+                totalWater += rmax - arr[j];
+                j--;
+            }
+        }
+        return totalWater;
+    
+    }
+}
