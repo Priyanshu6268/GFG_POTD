108716e997b7e6f0a0dc97cdca45c015b9c1c5b9
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-01-16 09:37:55 +0530

    Create 16Jan25.cpp

---
+++ 2025/01 - JAN/16Jan25.cpp
@@ -0,0 +1,22 @@
+//    https://www.geeksforgeeks.org/problems/largest-subarray-of-0s-and-1s/1
+class Solution {
+  public:
+    int maxLen(vector<int> &arr) {
+        // Your code here
+        int n = arr.size();
+        int ans = 0;
+        int sum = 0;
+        unordered_map<int,int> ump;
+        for(int i = 0;i < n;i++){
+            if(arr[i] == 0)
+                sum -= 1;
+            else 
+                sum +=1;
+            if(sum == 0 || ump[sum])
+                ans = max(ans,i-ump[sum]+1);
+            else if(!ump[sum])
+                ump[sum] = i+1;
+        }
+        return ans;
+    }
+};
