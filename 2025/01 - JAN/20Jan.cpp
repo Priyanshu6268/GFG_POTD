5819b39c1bbc3f3e0438fbfde78a67c922f43005
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-01-20 07:31:54 +0530

    Create 20Jan.cpp

---
+++ 2025/01 - JAN/20Jan.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    Node* sortedMerge(Node* head1, Node* head2) {
+        // code here
+        if(!head1 && !head2)
+            return NULL;
+        if(!head1)
+            return head2;
+        if(!head2)
+            return head1;
+        
+        if(head1->data < head2->data){
+            head1->next = sortedMerge(head1->next,head2);
+            return head1;
+        }
+        else{
+            head2->next = sortedMerge(head1,head2->next);
+            return head2;
+        }
+        
+        return NULL;
+        
+    }
+};
