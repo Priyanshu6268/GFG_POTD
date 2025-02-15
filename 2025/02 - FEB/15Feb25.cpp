583de1185cf3db0cd7b7e0cb18f1401622a7ebc0
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-02-15 06:31:50 +0530

    Create 15Feb25.cpp

---
+++ 2025/02 - FEB/15Feb25.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    void pushing(Node* root, Node* n1, Node* n2, int& ans) {
+        if (!root) return;
+        if (root->data>n1->data && root->data>n2->data) {
+            pushing(root->left,n1,n2,ans);
+            return;
+        }
+        else if (root->data<n1->data && root->data<n2->data) {
+            pushing(root->right,n1,n2,ans);
+            return;
+        }
+        ans = min(ans,root->data);
+        
+    }
+  public:
+    Node* LCA(Node* root, Node* n1, Node* n2) {
+        int lowest = INT_MAX;
+        //if (froot->data>=n1 && root->data>=n2) return min(n1,n2);
+        pushing(root,n1,n2,lowest);
+        Node* ans = new Node(lowest);
+        return ans;
+        
+    }
+};
