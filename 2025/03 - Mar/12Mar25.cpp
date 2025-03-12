e1ce9cc13ddaecb0f197bc2c6a286f9dc3b74891
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-03-12 14:49:32 +0530

    Create 12Mar25.cpp

---
+++ 2025/03 - Mar/12Mar25.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        // Write your code here
+        int n = cost.size();
+        int dp[n+2] = {0};
+        for(int i=n-1;i>=0;i--){
+            dp[i] = cost[i]+min(dp[i+1],dp[i+2]);
+        }
+        return min(dp[0],dp[1]);
+    }
+};
