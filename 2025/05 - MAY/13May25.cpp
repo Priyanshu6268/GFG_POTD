44d9a20a434a16f24d83ccd6f5e71072ea32d28a
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-05-13 09:04:31 +0530

    Create 13May25.cpp

---
+++ 2025/05 - MAY/13May25.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int nCr(int n, int r) {
+        // code here
+        if(r>n) return 0;
+        long res=1;
+        for(int i=0;i<n-r;i++){
+            res=res*(n-i);
+            res=res/(i+1);
+        }
+        return (int)res;
+    }
+};
