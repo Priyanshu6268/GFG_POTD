0174633ebd9bed4a58d0caf212bf05397e5e5558
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-05-17 21:16:32 +0530

    Create 17May25.cpp

---
+++ 2025/05 - MAY/17May25.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    vector<int> sortArray(vector<int> &arr, int A, int B, int C) {
+        // code here
+        vector<int> v;
+        for (int x : arr) {
+            int calc = (A * x * x) + (B * x) + C;
+            v.push_back(calc);
+        }
+        sort(v.begin(), v.end());
+        return v;
+    }
+};
