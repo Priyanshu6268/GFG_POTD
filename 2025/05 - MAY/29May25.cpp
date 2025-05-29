2c868a3a3c2388c536ef45f6980e9176615ed932
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-05-29 09:28:15 +0530

    Create 29May25.cpp

---
+++ 2025/05 - MAY/29May25.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int ans=0;
+    void solve(Node* root,int& sum,int level,int& maxi){
+        if(root==NULL){
+            return ;
+        }
+        sum+=root->data;
+        if(level>maxi){
+            ans=sum;
+            maxi=level;
+        }
+        else if(level==maxi){
+            ans=max(ans,sum);
+        }
+        solve(root->left,sum,level+1,maxi);
+        solve(root->right,sum,level+1,maxi);
+        sum-=root->data;
+    }
+    int sumOfLongRootToLeafPath(Node *root) {
+        // code here
+        int sum=0,level=0,maxi=0;
+        solve(root,sum,level,maxi);
+        return ans;
+    }
+};
