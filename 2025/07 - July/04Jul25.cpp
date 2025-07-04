42daad38661cfda495ba68cc89bfb3bcd2dfa7eb
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-07-04 09:52:27 +0530

    Create 04Jul25.cpp

---
+++ 2025/07 - July/04Jul25.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int countAtMostK(vector<int> &arr, int k) {
+        int ans=0;
+        int s=0,e=0;
+        map<int,int>mp;
+        while(e<arr.size())
+        {
+            mp[arr[e]]++;
+            while(mp.size()>k)
+            {
+                mp[arr[s]]--;
+                if(!mp[arr[s]])mp.erase(arr[s]);
+                s++;
+            }
+            if(mp.size()<=k)ans+=e-s+1;
+            e++;
+        }
+        return ans;
+    }
+};
