a74df3cd55a1e3630c8bdbb02493219848a367a2
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-07-23 09:30:20 +0530

    Create 23Jul25.cpp

---
+++ 2025/07 - July/23Jul25.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int subarraySum(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        int sum=0;
+        
+        for(int i=0; i<n; i++){
+            int start=i+1;
+            int end=n-i;
+            int contri=start*end;
+            sum+=(arr[i]*contri);
+        }
+        
+        return sum;
+    }
+};
