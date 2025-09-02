c6492c2be99707b5c6b8f5a9609b0942467bcb7c
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-09-02 06:26:12 +0530

    Create 02Sep25.cpp

---
+++ 2025/09 - September/02Sep25.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    Node* swapKth(Node* head, int k) {
+        // code here
+        k--;
+        Node* temp = head;
+        while(temp and k--) {
+            temp = temp->next;
+        }
+        if(!temp) return head;
+        Node *first = temp;
+        Node* second = head;
+        while(temp->next) {
+            second = second->next; temp=temp->next;
+        }
+        swap(first->data, second->data);
+        return head;
+    }
+};
