702f4307f762f76dfe7b80848592c8124948700b
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-10-01 07:34:36 +0530

    Create 01Oct25.cpp

---
+++ 2025/10 - October/01Oct25.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    vector<vector<int>> uniquePerms(vector<int>& arr) {
+        vector<vector<int>> ans;
+        sort(arr.begin(),arr.end());
+        do{
+            ans.push_back(arr);
+        }while(next_permutation(arr.begin(),arr.end()));
+        return ans;
+    }
+};
