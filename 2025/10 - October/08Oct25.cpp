311ebb67123712eb1e8585c3c4703bbb89167c27
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-10-08 06:29:19 +0530

    Create 08Oct25.cpp

---
+++ 2025/10 - October/08Oct25.cpp
@@ -0,0 +1,41 @@
+/*
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* Solve(int &i, int &j, vector<int> &pre, vector<int> &post){
+        
+        Node* node = new Node(pre[i]);
+        i++;
+        
+        if(node->data == post[j]){
+            j++;
+            return node;
+        }
+        
+        Node* left = Solve(i, j, pre, post);
+        node->left = left;
+        Node* right = Solve(i, j, pre, post);
+        node->right = right;
+        
+        j++;
+        return node;
+    }
+    Node *constructTree(vector<int> &pre, vector<int> &post) {
+        int i = 0;
+        int j = 0;
+        return Solve(i, j, pre, post);
+    }
+};
