25d007e2f823076c9deb005f2c87086668e826bb
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-10-15 11:22:08 +0530

    Create 15Oct25.cpp

---
+++ 2025/10 - October/15Oct25.cpp
@@ -0,0 +1,33 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+    priority_queue<int> pq; 
+public:
+    void inorder(Node* root, int k) {
+        if (!root) return;
+
+        inorder(root->left, k);
+        pq.push(root->data);
+        if (pq.size() > k) pq.pop(); 
+        inorder(root->right, k);
+    }
+
+    int kthSmallest(Node *root, int k) {
+        inorder(root, k);
+        if(pq.size()==k)
+        return pq.top();
+        return -1;
+    }
+};
