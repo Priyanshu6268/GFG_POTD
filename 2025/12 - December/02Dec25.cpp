69877d02f78d64ce5fd83b1466cc27e9211cfd11
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-12-02 14:07:14 +0530

    Update 02Dec25.cpp

--- 2025/12 - December/02Dec25.cpp
+++ 2025/12 - December/02Dec25.cpp
@@ -1,58 +1,74 @@
-class Solution {
-public:
-    int solve(int ind, string &s,
-              vector<vector<int>> &adj,
-              vector<long long> &pref,
-              vector<vector<int>> &nextPos,
-              vector<long long> &dp) {
-
-        if (ind == (int)s.size() - 1) return 0;
-        if (dp[ind] != -1) return dp[ind];
-
-        long long best = 0;
-        int curr = (unsigned char)s[ind];
-
-        for (int target : adj[curr]) {
-
-            int j = nextPos[ind][target];
-            if (j == -1) continue;
-
-            long long gain = pref[j] - pref[ind];
-
-            for (int k = ind; k < j; k++)
-                if ((unsigned char)s[k] == target)
-                    gain -= target;
-
-            gain += solve(j, s, adj, pref, nextPos, dp);
-            best = max(best, gain);
+class Solution
+{
+    public:
+        vector<vector<int>> adj;
+        vector<int> pref;
+        vector<vector<int>> arr;
+        vector<int> DP;
+        vector<vector<int>> Next;
+        int Solve(int i, string &s)
+        {
+            int n = s.size();
+            if (i >= n)
+                return 0;
+            if (DP[i] != -1)
+                return DP[i];
+            int ans = 0;
+            for (int j = 0; j < 26; j++)
+            {
+                if (adj[s[i] - 'a'][j] == 0)
+                    continue;
+                int idx = upper_bound(Next[j].begin(), Next[j].end(), i) - Next[j].begin();
+                if (idx == Next[j].size())
+                    continue;
+                int x = Next[j][idx];
+                int cost = pref[x] - ((i > 0) ? pref[i - 1] : 0);
+                int cnt = arr[j][x] - ((i > 0) ? arr[j][i - 1] : 0);
+                int p = int(char(j + 'a'));
+                cost -= p * cnt;
+                ans = max(ans, cost + Solve(x, s));
+            }
+            return DP[i] = ans;
         }
-        return dp[ind] = best;
-    }
-
-    int maxScore(string &s, vector<vector<char>> &jumps) {
-        int n = s.size();
-
-        for (int c = 0; c < 256; c++)
-            jumps.push_back({(char)c, (char)c});
-
-        vector<vector<int>> nextPos(n, vector<int>(256, -1));
-        vector<int> last(256, -1);
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int c = 0; c < 256; c++)
-                nextPos[i][c] = last[c];
-            last[(unsigned char)s[i]] = i;
-        }
-
-        vector<vector<int>> adj(256);
-        for (auto &v : jumps)
-            adj[(unsigned char)v[0]].push_back((unsigned char)v[1]);
-
-        vector<long long> pref(n + 1, 0);
-        for (int i = 0; i < n; i++)
-            pref[i + 1] = pref[i] + (unsigned char)s[i];
-
-        vector<long long> dp(n, -1);
-        return solve(0, s, adj, pref, nextPos, dp);
+        int maxScore(string &s, vector<vector<char>> &jumps)
+        {
+            int n = s.size(), m = jumps.size();
+            if (n == 1)
+                return 0;
+            adj.assign(26, vector<int>(26, 0));
+            for (int i = 0; i < 26; i++)
+                adj[i][i] = 1;
+            for (int i = 0; i < m; i++)
+            {
+                int u = jumps[i][0] - 'a';
+                int v = jumps[i][1] - 'a';
+                adj[u][v] = 1;
+            }
+            pref.assign(n, 0);
+            for (int i = 0; i < n; i++)
+            {
+                int x = int(s[i]);
+                pref[i] = x;
+                if (i > 0)
+                    pref[i] += pref[i - 1];
+            }
+            arr.assign(26, vector<int>(n, 0));
+            for (int j = 0; j < n; j++)
+            {
+                int x = s[j] - 'a';
+                for (int i = 0; i < 26; i++)
+                {
+                    if (j > 0)
+                        arr[i][j] += arr[i][j - 1];
+                    if (x == i)
+                        arr[i][j]++;
+                }
+            }
+            Next.assign(26, vector<int>());
+            for (int i = 0; i < n; i++)
+                Next[s[i] - 'a'].push_back(i);
+            DP.assign(n, -1);
+            int ans = Solve(0, s);
+            return ans;
     }
 };
