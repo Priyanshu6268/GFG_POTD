5a752e4aa5d3c49496f95f68b076ac9ca1d51597
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-12-12 11:26:36 +0530

    Update 12Dec25.cpp

--- 2025/12 - December/12Dec25.cpp
+++ 2025/12 - December/12Dec25.cpp
@@ -1,13 +1,17 @@
 class Solution {
   public:
     vector<vector<int>> transpose(vector<vector<int>>& mat) {
-        // code here
-        int n=mat.size();
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                swap(mat[i][j], mat[j][i]);
+        if (mat.empty() || mat[0].empty()) return {};
+
+        int m = mat.size();
+        int n = mat[0].size();
+
+        vector<vector<int>> res(n, vector<int>(m));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                res[j][i] = mat[i][j];
             }
         }
-        return mat;
+        return res;
     }
 };
