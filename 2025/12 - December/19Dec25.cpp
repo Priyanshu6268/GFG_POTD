e40d7b14d50b311568957fb02e4d6bc22651ada4
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-12-19 09:31:03 +0530

    Create 19Dec25.cpp

---
+++ 2025/12 - December/19Dec25.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int findMoves(vector<int>& chairs, vector<int>& passengers) {
+        // code here
+        sort(chairs.begin(), chairs.end());
+        sort(passengers.begin(), passengers.end());
+        
+        int n=chairs.size();
+        int ans=0;
+        
+        for(int i=0; i<n; i++){
+            ans+=abs(passengers[i]-chairs[i]);
+        }
+        
+        return ans;
+    }
+};
