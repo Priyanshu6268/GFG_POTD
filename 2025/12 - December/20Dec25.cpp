4640f38ef01c9ae69aaa8a2e98b5ccb5f2b0cb95
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-12-20 08:36:39 +0530

    Create 20Dec25.cpp

---
+++ 2025/12 - December/20Dec25.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int searchInsertK(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        int i=0,j=n-1;
+        while(i<=j){
+            int m = i + (j-i)/2;
+            if(arr[m]==k)return m;
+            if(arr[m]>k)j=m-1;
+            else i=m+1;
+        }
+        return i;
+    }
+};
